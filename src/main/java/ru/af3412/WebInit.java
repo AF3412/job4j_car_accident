387202189d1d50438a7865944d608c558b27b17e
alex.filatov <alex.filatov@sparox.ru>
2021-04-30 18:07:47 +0300

    0. Spring Security[#6879#527378]

--- src/main/java/ru/af3412/WebInit.java
+++ src/main/java/ru/af3412/WebInit.java
@@ -4,8 +4,8 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
-import ru.af3412.accident.config.DataConfig;
 import ru.af3412.accident.config.WebConfig;
+import ru.af3412.accident.security.SecurityConfig;
 
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
@@ -15,7 +15,7 @@ public class WebInit implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletCxt) {
         AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-        ac.register(WebConfig.class, DataConfig.class);
+        ac.register(WebConfig.class, SecurityConfig.class);
         ac.refresh();
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("UTF-8");
---
+++ src/main/java/ru/af3412/accident/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package ru.af3412.accident.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
