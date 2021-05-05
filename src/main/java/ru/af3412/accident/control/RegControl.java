c46d35aab7dd373ac117d5522585ec18e444554d
alex.filatov <alex.filatov@sparox.ru>
2021-05-05 11:48:22 +0300

    2. Регистрация пользователя[#296069#528855] fix

--- src/main/java/ru/af3412/accident/control/RegControl.java
+++ src/main/java/ru/af3412/accident/control/RegControl.java
@@ -2,13 +2,17 @@ package ru.af3412.accident.control;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.af3412.accident.model.Accident;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.af3412.accident.model.User;
 import ru.af3412.accident.repository.AuthorityRepository;
 import ru.af3412.accident.repository.UserRepository;
+
+import java.text.MessageFormat;
+import java.util.Optional;
 
 @Controller
 public class RegControl {
@@ -25,6 +29,11 @@ public class RegControl {
 
     @PostMapping("/reg")
     public String save(@ModelAttribute User user) {
+        Optional<User> findUser = users.findByUsername(user.getUsername());
+        if (findUser.isPresent()) {
+            String error = MessageFormat.format("User with name \"{0}\" is exists!", user.getUsername());
+            return "redirect:/reg?error=true&errorMessage=" + error;
+        }
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
@@ -33,7 +42,12 @@ public class RegControl {
     }
 
     @GetMapping("/reg")
-    public String reg(@ModelAttribute Accident accident) {
+    public String reg(@RequestParam(value = "error", required = false) String error,
+                      @RequestParam(value = "errorMessage", required = false) String errorMessage,
+                      Model model) {
+        if (error != null) {
+            model.addAttribute("errorMessage", errorMessage);
+        }
         return "reg";
     }
 }
